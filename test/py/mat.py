00d4d9e1c8a69151cba1fe90e9926ac84779f275
Vanessa McHale <vamchale@gmail.com>
2024-10-27 12:27:35 -0400

    catch errors

--- test/py/mat.py
+++ test/py/mat.py
@@ -1,19 +1,20 @@
 import numpy as np;import apple
 
+from numpy.testing import assert_array_equal,assert_allclose
 def vs(M,N):
     A=np.random.rand(M,N);x=np.random.rand(N)
     v=apple.jit(f"[(x::Arr ({M}×{N}) float)%:y]")
-    print(A@x);print(v(A,x))
+    assert_allclose(A@x,v(A,x),rtol=1e-15,strict=True)
 
 vs(100,32)
 def test(M,N,K):
     bs=np.random.rand(M,N);cs=np.random.rand(N,K)
     m=apple.jit(f"[(x::Arr ({M}×{N}) float)%.(y::Arr ({N}×{K}) float)]")
-    assert (bs@cs==m(bs,cs)).all()
+    assert_array_equal(bs@cs,m(bs,cs))
 
     ds=np.random.rand(K,N)
     mT=apple.jit(f"[(x::Arr ({M}×{N}) float)%.|:(y::Arr ({K}×{N}) float)]")
-    print(bs@ds.T);print(mT(bs,ds))
+    assert_allclose(bs@ds.T,mT(bs,ds),rtol=1e-10,strict=True)
 
 test(10000,784,128)
 test(257,32,32)
