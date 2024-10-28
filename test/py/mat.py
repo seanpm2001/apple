274e960aae48ecc64de5b3e9720c0608464bf8bb
Vanessa McHale <vamchale@gmail.com>
2024-10-28 05:14:20 -0400

    one

--- test/py/mat.py
+++ test/py/mat.py
@@ -10,11 +10,13 @@ vs(100,32)
 def test(M,N,K):
     bs=np.random.rand(M,N);cs=np.random.rand(N,K)
     m=apple.jit(f"[(x::Arr ({M}×{N}) float)%.(y::Arr ({N}×{K}) float)]")
-    assert_array_equal(bs@cs,m(bs,cs))
+    assert_allclose(bs@cs,m(bs,cs),rtol=1e-10,strict=True)
 
     ds=np.random.rand(K,N)
     mT=apple.jit(f"[(x::Arr ({M}×{N}) float)%.|:(y::Arr ({K}×{N}) float)]")
     assert_allclose(bs@ds.T,mT(bs,ds),rtol=1e-10,strict=True)
 
+test(4,512,8);test(2,100,16)
 test(10000,784,128)
+test(100,2,15)
 test(257,32,32)
